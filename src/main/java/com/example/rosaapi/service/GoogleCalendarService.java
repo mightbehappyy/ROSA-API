286715b8b693225cc308824d8fd9bf07fd2a6c80
Pedro <pedroluiz0201690@gmail.com>
2023-12-10 02:44:33 -0300

    refac: Changes made to  GoogleCalendarService and GoogleAuthService file added

--- src/main/java/com/example/rosaapi/service/GoogleCalendarService.java
+++ src/main/java/com/example/rosaapi/service/GoogleCalendarService.java
@@ -3,103 +3,46 @@ package com.example.rosaapi.service;
 import com.example.rosaapi.model.dtos.CalendarEventDTO;
 import com.example.rosaapi.model.dtos.CalendarWeekEventsDTO;
 import com.example.rosaapi.utils.DateTimeUtils;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
 
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.time.*;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-/* class to demonstrate use of Calendar events list API */
+import static com.example.rosaapi.service.GoogleAuthService.*;
+
 public class GoogleCalendarService {
 
-    private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
 
-    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static Calendar getCalendarService() throws GeneralSecurityException, IOException {
+        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
 
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-    private static final List<String> SCOPES =
-            Collections.singletonList(CalendarScopes.CALENDAR);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-
-
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
-            throws IOException {
-        // Load client secrets.
-        InputStream in = GoogleCalendarService.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
+        return new Calendar.Builder(httpTransport, JSON_FACTORY, getCredentials(httpTransport))
+                .setApplicationName(APPLICATION_NAME)
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        //returns an authorized Credential object.
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
     public CalendarWeekEventsDTO getWeekEvents() throws IOException, GeneralSecurityException {
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+        Calendar service = getCalendarService();
 
-
-        ZoneId utcMinus3 = ZoneId.of("UTC-3");
-
-        ZonedDateTime currentDateTime = ZonedDateTime.now(utcMinus3);
-
-
-        LocalDate startOfWeek;
-        if (currentDateTime.getDayOfWeek() == DayOfWeek.SATURDAY || currentDateTime.getDayOfWeek() == DayOfWeek.SUNDAY)
-        {
-            startOfWeek = currentDateTime.with(TemporalAdjusters.next(DayOfWeek.MONDAY)).toLocalDate();
-        } else {
-            startOfWeek = currentDateTime.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)).toLocalDate();
-        }
-
+        LocalDate startOfWeek = getStartOfWeek();
         LocalDate endOfWeek = startOfWeek.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY));
 
-
-        DateTime startTime = new DateTime(startOfWeek.atStartOfDay().toInstant(ZoneOffset.ofHours(-3)).toEpochMilli());
-        DateTime endTime = new DateTime(endOfWeek.plusDays(1).atStartOfDay().toInstant(ZoneOffset.ofHours(-3))
+        DateTime startTime = new DateTime(startOfWeek.atStartOfDay().toInstant(ZoneOffset.ofHours(-3))
+                .toEpochMilli());
+        DateTime endTime = new DateTime(endOfWeek.atStartOfDay().toInstant(ZoneOffset.ofHours(-3))
                 .toEpochMilli());
 
-
-
-        Events events = service.events().list("primary")
-                .setTimeMin(startTime)
-                .setTimeMax(endTime)
-                .setOrderBy("startTime")
-                .setSingleEvents(true)
-                .execute();
+        Events events = fetchEvents(service, startTime, endTime);
 
         return getCalendarWeekEventsDTO(events);
     }
@@ -113,16 +56,30 @@ public class GoogleCalendarService {
             long start = event.getStart().getDateTime().getValue();
             long end = event.getEnd().getDateTime().getValue();
             String summary = event.getSummary();
-
             CalendarEventDTO calendarEventDTO = DateTimeUtils.convertUnixToDTO(start, end, summary);
-
             calendarEventDTOS.add(calendarEventDTO);
-
         }
 
         calendarWeekEvents.setWeekEvents(calendarEventDTOS);
         return calendarWeekEvents;
     }
 
+    private static Events fetchEvents(Calendar service, DateTime startTime, DateTime endTime) throws IOException {
+        return service.events().list("primary")
+                .setTimeMin(startTime)
+                .setTimeMax(endTime)
+                .setOrderBy("startTime")
+                .setSingleEvents(true)
+                .execute();
+    }
 
+    private static LocalDate getStartOfWeek() {
+        ZonedDateTime currentDateTime = ZonedDateTime.now();
+        if (currentDateTime.getDayOfWeek() == DayOfWeek.SATURDAY || currentDateTime.getDayOfWeek() == DayOfWeek.SUNDAY)
+        {
+            return currentDateTime.with(TemporalAdjusters.next(DayOfWeek.MONDAY)).toLocalDate();
+        } else {
+            return currentDateTime.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)).toLocalDate();
+        }
+    }
 }
