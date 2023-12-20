fc29fd7e4a88a2e1c9f0fab3a4cfd1a0618358e9
Pedro <pedroluiz0201690@gmail.com>
2023-12-20 00:59:23 -0300

    feat: Changed from installed app flow to service account authentication

--- src/main/java/com/example/rosaapi/service/GoogleAuthService.java
+++ src/main/java/com/example/rosaapi/service/GoogleAuthService.java
@@ -1,71 +1,45 @@
 package com.example.rosaapi.service;
 
-import com.example.rosaapi.utils.exceptions.CalendarCredentialsException;
 import com.example.rosaapi.utils.exceptions.CalendarServiceException;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.auth.oauth2.GoogleCredentials;
 import org.springframework.stereotype.Service;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
-import java.util.List;
 
 @Service
 public class GoogleAuthService {
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-    private static final List<String> SCOPES =
-            Collections.singletonList(CalendarScopes.CALENDAR);
-    private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+    private static final String CREDENTIALS_FILE_PATH = "src/main/resources/credentials.json";
+    private static final String APPLICATION_NAME = "ROSA-API";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private GoogleAuthService() {
 
-    }
-    private static Credential getCredentials(final NetHttpTransport httpTransport) {
-        try {
-            // Load client secrets.
-            InputStream in = GoogleCalendarService.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-            if (in == null) {
-                throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-            }
-            GoogleClientSecrets clientSecrets =
-                    GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-            // Build flow and trigger user authorization request.
-            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                    httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
-                    .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                    .setAccessType("offline")
-                    .build();
-            LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-            //returns an authorized Credential object.
-            return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-        } catch(IOException e) {
-            throw  new CalendarCredentialsException("An error occurred:" + e);
-        }
     }
 
     public static Calendar getCalendarService() {
         try{
             final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
 
-            return new Calendar.Builder(httpTransport, JSON_FACTORY, getCredentials(httpTransport))
+            GoogleCredentials serviceAccountCredentials = ServiceAccountCredentials
+                    .fromStream(new FileInputStream(CREDENTIALS_FILE_PATH))
+                    .createScoped(Collections.singletonList(CalendarScopes.CALENDAR));
+
+            HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(serviceAccountCredentials);
+
+            return new Calendar
+                    .Builder(httpTransport, JSON_FACTORY, requestInitializer)
                     .setApplicationName(APPLICATION_NAME)
                     .build();
+
         } catch(IOException | GeneralSecurityException e) {
             throw new CalendarServiceException("An error occurred:"+e);
         }
